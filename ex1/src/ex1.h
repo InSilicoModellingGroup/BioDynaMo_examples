5d43d4cd38565c040d5fba00ad0375803826a7f8
Vasileios Vavourakis <vasvav@gmail.com>
2025-06-11 10:10:13 +0300

    Added example simulation #1

---
+++ ex1/src/ex1.h
@@ -0,0 +1,61 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (C) 2021 CERN & University of Surrey for the benefit of the
+// BioDynaMo collaboration. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// See the LICENSE file distributed with this work for details.
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.
+//
+// -----------------------------------------------------------------------------
+#ifndef EX1_H_
+#define EX1_H_
+
+#include "biodynamo.h"
+
+namespace bdm {
+
+inline int ex1(int argc, const char* argv[]) {
+  // https://biodynamo.github.io/api/structbdm_1_1Param.html
+  auto set_parameters = [](Param* param) {
+    param->use_progress_bar = true;
+    param->bound_space = Param::BoundSpaceMode::kOpen;
+    param->min_bound = -100.0;
+    param->max_bound = +100.0;
+    param->export_visualization = true;
+    param->visualization_interval = 1;
+    param->visualize_agents["Cell"] = { "diameter_" };
+    param->statistics = false;
+    param->simulation_time_step = 1.0;
+  };
+
+  // https://biodynamo.github.io/api/classbdm_1_1Simulation.html
+  Simulation sim(argc, argv, set_parameters);
+  // https://biodynamo.github.io/api/classbdm_1_1ResourceManager.html
+  auto* rm = sim.GetResourceManager();
+  // https://biodynamo.github.io/api/classbdm_1_1Random.html
+  auto* rand = sim.GetRandom();
+
+  const Real3 xyz{rand->Uniform(-10.,+10.),
+                  rand->Uniform(-10.,+10.),
+                  rand->Uniform(-10.,+10.)};
+
+  // https://biodynamo.github.io/api/classbdm_1_1Cell.html
+  Cell* cell = new Cell({0.0, 0.0, 0.0});
+  cell->SetDiameter(2.0);
+  cell->SetDensity(1.0);
+  cell->SetPosition(xyz);
+  rm->AddAgent(cell);
+
+  sim.GetScheduler()->Simulate(10);
+
+  std::cout << "Simulation completed successfully!" << std::endl;
+  return 0;
+}
+
+}  // namespace bdm
+
+#endif  // EX1_H_
